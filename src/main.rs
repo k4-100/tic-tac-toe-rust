48830d5495049b645c3137441bea5f433adb4570
k4-100 <k4-100@protonmail.com>
2022-09-25 22:42:33 +0200

    Added player sign deciding

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::io::{self, Write};
+mod utl;
 
 fn main() {
-  let mut input: String = String::new();
-  println!("WELCOME TO APP!");
-  print!("Name: ");
-  io::stdout().flush().expect("error: failed to flush after print!");
-  let stdin = io::stdin().read_line(&mut input).expect("error: can't read console input");
-  println!("\n\nWelcome {} !", input.trim());
+  let player_sign = utl::get_player_sign();
+  println!("{:?}", player_sign);
 }
---
+++ src/utl.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write};
+
+#[derive(Debug)]
+pub enum PlayerSign {
+  X,
+  O,
+  Null
+}
+
+pub fn get_player_sign() -> PlayerSign {
+  let mut player_sign: PlayerSign = PlayerSign::Null;
+  let mut buf: String;
+
+
+  while matches!(player_sign, PlayerSign::Null){
+    buf = String::new();
+    print!("Pick X or O: ");
+    io::stdout().flush().expect("error: failed to flush");
+    io::stdin().read_line(&mut buf).expect("error: can't read console input");
+
+    match buf.as_str().trim(){
+      "X" | "x" => player_sign = PlayerSign::X,
+      "O" | "o" => player_sign = PlayerSign::O,
+      _=> {
+        println!("Wrong input, try again");
+      }
+    }
+  }
+  
+  return player_sign 
+} 
