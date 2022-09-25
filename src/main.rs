14b39e2fc4fbf4390080b174d24debbf10e2339c
k4-100 <k4-100@protonmail.com>
2022-09-25 19:11:34 +0200

    Created basic output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+  let mut input: String = String::new();
+  println!("WELCOME TO APP!");
+  print!("Name: ");
+  io::stdout().flush().expect("error: failed to flush after print!");
+  let stdin = io::stdin().read_line(&mut input).expect("error: can't read console input");
+  println!("\n\nWelcome {} !", input.trim());
 }
