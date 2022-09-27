c6fe49b61fbb3729b123019b9cf21c40f6d95b03
k4-100 <k4-100@protonmail.com>
2022-09-27 23:02:34 +0200

    Made getBoard take array as an argument

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ mod utl;
 fn main() {
   let player_sign: utl::PlayerSign= utl::get_player_sign();
   println!("{:?}", player_sign);
+
   let current_player: utl::PlayerSign = player_sign;
+  let board : [&str; 9] = ["0","1","2","3","4","5","6","7","8"];
   while true{
 
+
     // "render"
-    println!("{}", utl::get_board() );
+    println!("{}", utl::get_board(board) );
   }
   
 }
--- src/utl.rs
+++ src/utl.rs
@@ -30,16 +30,22 @@ pub fn get_player_sign() -> PlayerSign {
   return player_sign 
 } 
 
+// pub fn change_player
 
 
-pub fn get_board() -> String{
+pub fn get_board(board: [&str; 9] ) -> String{
+  return format!("     
+          ###########
+          #{b0}###{b1}###{b2}#
+          ###########
+          #{b3}###{b4}###{b5}#
+          ###########
+          #{b6}###{b7}###{b8}#
+          ###########", 
+          b0 = board[0], b1 = board[1], b2 = board[2],
+          b3 = board[3], b4 = board[4], b5 = board[5],
+          b6 = board[6], b7 = board[7], b8 = board[8],
+  );
 
-  return String::from("
-          ###########
-          #X###X###X#
-          ###########
-          #X###X###X#
-          ###########
-          #X###X###X#
-          ###########")
+
 }
