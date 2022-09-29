69730232c3184712b386463bf899d9f7368cd5ab
k4-100 <k4-100@protonmail.com>
2022-09-29 18:27:27 +0200

    Added working "AI"

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use crate::utl::get_board;
+use rand::prelude::*;
 
 mod utl;
 
 fn main() {
-  let player_sign: utl::PlayerSign= utl::get_player_sign();
-  println!("{:?}", player_sign);
+  let human_player: utl::PlayerSign= utl::get_player_sign();
+  println!("{:?}", human_player);
 
-  let mut current_player: utl::PlayerSign = player_sign;
+  let mut current_player: utl::PlayerSign = human_player.clone();
   let mut board : [&str; 9] = [
     "*", "*", "*",
     "*", "*", "*",
     "*", "*", "*",
   ];
+  let mut rounds: u8 = board.len() as u8;
+  
+  // println!("{}",rand::thread_rng(). gen_range(1..9));
 
   loop {
-
-    utl::perform_round(&mut board, &mut current_player);
+    // input from player/computer
+    utl::perform_round(&mut board, &human_player, &mut current_player, &mut rounds );
 
     // "render"
     println!("{}", utl::get_board(board) );
--- src/utl.rs
+++ src/utl.rs
@@ -1,12 +1,14 @@
-use std::io::{self, Write};
-use::std::num::{ParseIntError};
+use std::{io::{self, Write}};
+use std::num::{ParseIntError};
+use rand::prelude::*;
 
-#[derive(Debug)]
+#[derive(Debug,Clone, PartialEq)]
 pub enum PlayerSign {
   X,
   O,
   Null
 }
+
 
 pub fn get_player_sign() -> PlayerSign {
   let mut player_sign: PlayerSign = PlayerSign::Null;
@@ -32,32 +34,51 @@ pub fn get_player_sign() -> PlayerSign {
 } 
 
 
-pub fn perform_round( board: &mut [&str; 9], player_sign: &mut PlayerSign   ){
+pub fn perform_round( board: &mut [&str; 9], human_player: &PlayerSign, current_player:  &mut PlayerSign, rounds: &mut u8 ){
+  let mut passed: bool = false;
   let mut buf: String = String::new();
-
-  print!("pick a spot: ");
-  io::stdout().flush().expect("failed to flush");
-  io::stdin().read_line(&mut buf).expect("failed to readline");
-
-  let buf_num_res: Result<usize, ParseIntError> = buf.trim().parse::<usize>();
-  let mut buf_num: usize = match buf_num_res {
-    Ok(v) => v,
-    Err(_)=> {
-      println!("Failed to parse to usize");
-      0
-    }
-  };
+  let mut buf_num: usize;
   
-  match buf_num{
-    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => {
-      if board[buf_num-1] == "*"{
-        board[buf_num-1] = "X";
-      }
-      else{
-        println!("index already in issue");
-      }
+  while !passed {
+    if human_player == current_player{
+      print!("pick a spot: ");
+      io::stdout().flush().expect("failed to flush");
+      io::stdin().read_line(&mut buf).expect("failed to readline");
+      let buf_num_result: Result<usize, ParseIntError> = buf.trim().parse::<usize>();
+      buf_num = match buf_num_result {
+        Ok(v) => v,
+        Err(_)=> {
+          println!("Failed to parse to usize");
+          0
+        }
+      };
     }
-    _ => println!("not an index")
+    else {
+      // println!("AI TIME");
+      buf_num = rand::thread_rng().gen_range(1..9);
+    }
+    
+    match buf_num{
+      1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => {
+        if board[buf_num-1] == "*"{
+          
+          if matches!(current_player, PlayerSign::X){
+
+            board[buf_num-1] = "X";
+            *current_player = PlayerSign::O;
+          }else{
+            board[buf_num-1] = "O";
+            *current_player = PlayerSign::X;
+          }
+          *rounds -= 1;
+          passed = true;
+        }
+        else{
+          println!("index already taken");
+        }
+      }
+      _ => println!("not an index")
+    }
   }
 }
 
